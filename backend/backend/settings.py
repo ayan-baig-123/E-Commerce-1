8e79581af23c37fe6c42fe7cc577db0e7ec69b4b
ayan-baig-123 <ayanbaig@iak.ngo>
2026-05-16 12:59:58 +0500

    Fix error for frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,5 +147,18 @@ EMAIL_HOST_PASSWORD = 'xotf wwpo xhfi njmc'  # Gmail ka App Password
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 
 
